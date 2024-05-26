8bace6ddea3140c37581e8b170ca7d1b561d16b9
Artiom <artiomkrupski@mail.ru>
2024-05-27 00:04:47 +0300

    remove unnessary files

--- lab1/main.py
+++
@@ -1,9 +0,0 @@
-from financial_state_machine import FinancialStateMachine
-from repository.file_repository import FileRepository, Repository
-
-repository: Repository = FileRepository("appstate.pickle")
-
-state_machine = FinancialStateMachine(repository)
-
-while True:
-    state_machine.move_next(input())
