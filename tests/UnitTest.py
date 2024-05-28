690a78af14e21c0c50e1ce3eb045f7354c4e0a3b
ArtemPetrashenko <117662175+ArtemPetrashenko@users.noreply.github.com>
2024-05-28 11:35:20 +0300

    Update UnitTest.py

--- tests/UnitTest.py
+++ tests/UnitTest.py
@@ -1,1 +1,19 @@
+import unittest
+import coverage
+
+
+def run_tests_with_coverage():
+    cov = coverage.Coverage()
+    cov.start()
+
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover("../test", pattern="*Test.py")
+    unittest.TextTestRunner(verbosity=2).run(test_suite)
+
+    cov.stop()
+    cov.report()
+
+
+if __name__ == "__main__":
+    run_tests_with_coverage()
 
