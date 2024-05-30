d34db43fcab4314cdcbc4e403b28ea2454f27fd6
Elize3000 <elize30000@mail.ru>
2024-05-31 02:02:13 +0300

    small changes for documintation

--- Lab_1_Garden/garden.py
+++ Lab_1_Garden/garden.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import markdown
 
 class Garden:
 
@@ -11,6 +12,7 @@ class Garden:
     """"Function for adding garden bed"""
 
     def add_garden_bed(self, length, width, soil_typ):
+        """"Function for adding garden bed"""
         id_ = len(self.garden_plan)
         try:
             length = abs(int(length))
@@ -21,9 +23,10 @@ class Garden:
         self.garden_plan.append(Garden_bed(length, width, soil_typ, id_))
         return 0
 
-    """"Function for watering plants"""
+
 
     def water_plants(self, id_):
+        """"Function for watering plants"""
         try:
             id_ = int(id_)
             self.garden_plan[id_]
@@ -31,9 +34,10 @@ class Garden:
             return -1
         return self.garden_plan[id_].watering(self)
 
-    """"Function for taking care of plants"""
+
 
     def take_care_of_plant(self, id_):
+        """"Function for taking care of plants"""
         try:
             id_ = int(id_)
             self.garden_plan[id_]
@@ -41,9 +45,10 @@ class Garden:
             return -1
         return  self.garden_plan[id_].take_care(self)
 
-    """"Function for fertilizing plants"""
+
 
     def fertilizing_lant(self, id_):
+        """"Function for fertilizing plants"""
         try:
             id_ = int(id_)
             self.garden_plan[id_]
@@ -51,18 +56,20 @@ class Garden:
             return -1
         return self.garden_plan[id_].fertilizing(self)
 
-    """"This function returns free beds"""
+
 
     def get_free_garden_beds(self):
+        """"This function returns free beds"""
         free_garden_beds_list = []
         for bed in self.garden_plan:
             if bed.get_plant == '':
                 free_garden_beds_list.append(bed)
         return free_garden_beds_list
 
-    """"This function returns free beds"""
+
 
     def get_solid_type_free_beds(self, solid):
+        """"This function returns free beds"""
         try:
             self.solid_type.index(solid)
         except:
@@ -74,6 +81,7 @@ class Garden:
         return solid_type_free_beds
 
     def get_planted_beds(self):
+        """"This function returns planted beds"""
         planted_beds = []
         for bed in self.garden_plan:
             if bed.get_plant != '':
@@ -81,6 +89,7 @@ class Garden:
         return planted_beds
 
     def plant_garden_bed(self, id_, plant_name):
+        """"This function plants"""
         try:
             id_ = int(id_)
             self.garden_plan[id_]
@@ -90,6 +99,7 @@ class Garden:
         return self.garden_plan[id_].planting(plant_name)
 
     def collect_garden_bed(self, id_):
+        """"This function collects plants"""
         try:
             id_ = int(id_)
             self.garden_plan[id_]
@@ -116,18 +126,20 @@ class Garden_bed:
             return True
         return False
 
-    """"Inicializing garden bed size"""
+
 
     def __init__(self, length, width, soil_type, id_):
+        """"Inicializing garden bed size"""
         self.garden_bed_length = length
         self.garden_bed_width = width
         self.soil_type = soil_type
         self.id_ = id_
 
 
-    """"Function for planting garden bed"""
+
 
     def planting(self, plant_name):
+        """"Function for planting garden bed"""
         if self.is_planted == False:
             self.plant_name = plant_name
             self.is_planted = True
@@ -140,56 +152,64 @@ class Garden_bed:
             return -2
 
 
-    """"Function for watering garden bed"""
+
 
     def watering(self):
+        """"Function for watering garden bed"""
         if(self.is_planted):
             self.time_of_watering = dt.datetime.now()
             return 0
         return -1
 
-    """"Function for fertilizing garden bed"""
+
 
     def fertilizing(self):
+        """"Function for fertilizing garden bed"""
         if(self.is_planted):
             self.time_of_fertilizing = dt.datetime.now()
             return 0
         return -1
 
-    """"Function for taking care garden bed"""
+
 
     def take_care(self):
+        """"Function for taking care garden bed"""
         if(self.is_planted):
             self.time_of_taking_care = dt.datetime.now()
             return 0
         return -1
 
-    """"Function for collection plants"""
+
 
     def collect(self):
+        """"Function for collection plants"""
         if self.is_planted == True:
            self.is_planted = False
            return self.garden_bed_width*self.garden_bed_length, self.plant_name
         else:
             return 0, ''
 
-    """"Function for getting size"""
+
 
     def get_garden_bed_size(self):
+        """"Function for getting size"""
         return self.garden_bed_width * self.garden_bed_length
 
-    """"Function for getting id"""
+
 
     def get_garden_bed_id(self):
+        """"Function for getting id"""
         return self.id_
 
-    """Function for getting solid type"""
+
 
     def get_garden_bed_solid_type(self):
+        """Function for getting solid type"""
         return self.soil_type
 
-    """Function for getting plant name"""
+
     def get_plant(self):
+        """Function for getting plant name"""
         if(self.is_planted):
             return self.plant_name
         else:
