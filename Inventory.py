c297dbffb92c357fb3ad6740c2d213b6f072e9ae
Elize3000 <elize30000@mail.ru>
2024-05-30 20:55:21 +0300

    lab 1 and 2 added

--- Inventory.py
+++
@@ -1,9 +0,0 @@
-from Menu import Menu
-
-class Inventory(Menu):
-    
-    def __init__(self, ingridients=None) -> None:
-        super().__init__()
-        if ingridients:
-            for i in ingridients:
-                self.addItem(i)
--- lab1/repository/interfaces.py
+++
@@ -1,7 +0,0 @@
-class Repository:
-    def save(self, obj) -> None:
-        pass
-
-    def load(self):
-        pass
-    
