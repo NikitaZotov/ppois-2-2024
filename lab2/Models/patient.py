7f1a9c172f6df0a326e0bd7ccb2d27ae3ffcfdd4
Ivan Pernikov <66262869+pryanikx@users.noreply.github.com>
2024-06-04 11:55:54 +0300

    Create patient.py

---
+++ lab2/Models/patient.py
@@ -0,0 +1,8 @@
+class Patient:
+    def __init__(self, full_name, address, birth_date, appointment_date, doctor_name, conclusion):
+        self.full_name = full_name
+        self.address = address
+        self.birth_date = birth_date
+        self.appointment_date = appointment_date
+        self.doctor_name = doctor_name
+        self.conclusion = conclusion
